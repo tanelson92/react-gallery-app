47cb9ea1e21139badeab0377fdd448fe5a81e273
tanelson92 <trent@pegasuscart.com>
2022-07-14 20:09:51 -0500

    Update NotFound.js
    update search result page.

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,8 +2,8 @@ import React from 'react';
 const NotFound = () => {
     return (
         <ul><li className="not-found">
-            <h3>No Results Found</h3>
-            <p>You search did not return any results. Please try again.</p>
+            <h3>Oops! No Results</h3>
+            <p>Unfortunately the search did not return any results. Please try again.</p>
         </li></ul>
     );
 }
